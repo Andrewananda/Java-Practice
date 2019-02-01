ed4638b02ecf649b3f78c264eb2d1c9e932fc037
Andrew Ananda <andrewtiema@gmail.com>
2019-02-01 15:10:48 +0300

    initial

---
+++ src/Arrays.java
@@ -0,0 +1,9 @@
+public class Arrays {
+    public static void main(String[] args) {
+        System.out.println("Index\tValue");
+        int array[] = {32,64,18,40,9,10};
+        for (int counter=0;counter<array.length;counter++){
+            System.out.println(counter + "\t" + array[counter]);
+        }
+    }
+}
